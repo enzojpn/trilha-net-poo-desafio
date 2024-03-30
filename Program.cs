aab5c416d4fbc59ca0268e078136b9fe7d042901
enzojpn <enzo_jpn@hotmail.com>
2024-03-30 12:36:39 -0300

    desafio poo dotnet dio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("1234", "A42", "aj314r99", 4);
+Iphone iphone = new Iphone("4321", "iphone5", "135m0329", 8);
+
+nokia.ReceberLigacao();
+iphone.ReceberLigacao();
+
+nokia.InstalarAplicativo("Instragram");
+iphone.InstalarAplicativo("DetranSP");
+
+nokia.Ligar();
+iphone.Ligar();
